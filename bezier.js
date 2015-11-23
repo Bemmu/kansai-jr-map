74ff92379f38474d426f93561bf9fa8eaf79d2b1
Bemmu Sepponen <manga@bemmu.com>
2015-11-24 03:57:29 +0900

    Made it move at uniform speed

--- bezier.js
+++ bezier.js
@@ -8,7 +8,26 @@ function curveXY(p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y, t) {
 	}
 }
 
+// A bit of magic that gives the proper amount to increment t by to make 
+// movement along a cubic bezier curve have constant acceleration.
+// http://gamedev.stackexchange.com/questions/27056/how-to-achieve-uniform-speed-of-movement-on-a-bezier-curve
+function curveIncrement(p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y, t) {
+	var v1x = -3*p0x +  9*p1x  - 9*p2x + 3*p3x;
+	var v1y = -3*p0y +  9*p1y  - 9*p2y + 3*p3y;
+	var v2x =  6*p0x - 12*p1x  + 6*p2x;
+	var v2y =  6*p0y - 12*p1y  + 6*p2y;
+	var v3x = -3*p0x +  3*p1x;
+	var v3y = -3*p0y +  3*p1y;
+
+	var x = t*t*v1x + t*v2x + v3x;
+	var y = t*t*v1y + t*v2y + v3y;
+	var length = Math.sqrt(x*x + y*y);
+	return length;
+}
+
 var start = Date.now();
+var prev = Date.now();
+var t = 0;
 
 function drawStuff() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
@@ -26,7 +45,17 @@ function drawStuff() {
 	context.stroke();
 
 //	for (var t = 0.0; t < 1.0; t += 0.01) {
-	var t = (Date.now()-start)%1000/1000.;
+//	var t = (Date.now()-start)%1000/1000.;
+//	t += (Date.now() - prev)*0.001;
+
+	var elapsed = (Date.now() - prev)*0.001;
+	t = t + 2./curveIncrement(p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y, t);
+
+//	x*y*y = z
+
+	if (t > 1) t = 0;
+	prev = Date.now();
+
 	var p = curveXY(p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y, t);
 	context.fillStyle = 'white';
 	context.fillRect(-3 + p.x, -3 + p.y, 7, 7);
